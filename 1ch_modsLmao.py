ef8b3523f2f816e4fd0bf8b1d21d386948ee71a4
AinsworthNecco <155343690+AinsworthNecco@users.noreply.github.com>
2025-09-21 09:23:40 +0700

    Update 1ch_modsLmao.py

--- 1ch_modsLmao.py
+++ 1ch_modsLmao.py
@@ -1,7 +1,1 @@
-#!/data/data/com.termux/files/usr/bin/python
-# -*- coding: utf-8 -*-
-import os
-
-# Lệnh này trực tiếp tải và thực thi script từ GitHub với quyền root.
-os.system("curl -sSL https://raw.githubusercontent.com/AinsworthNecco/Lychkin/refs/heads/main/1ch_modsLmao.py | su -c 'bash -c \"/data/data/com.termux/files/usr/bin/python -\"'")
-
+curl -sSL https://raw.githubusercontent.com/AinsworthNecco/Lychkin/refs/heads/main/RejoinTool2.py | python
