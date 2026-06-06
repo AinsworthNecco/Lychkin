4c96d7a995557749b85c718b568a49cc57605c6f
AinsworthNecco <155343690+AinsworthNecco@users.noreply.github.com>
2026-06-06 11:09:19 +0700

    Update and rename RejoinTool4 to user.js

---
+++ user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bloxflip Auto Crash Pro (Launcher)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Trình khởi chạy load code trực tiếp từ GitHub
+// @author       You
+// @match        https://bloxflip.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bloxflip.com
+// @grant        none
+// @require      https://raw.githubusercontent.com/AinsworthNecco/Lychkin/refs/heads/main/Bloxflip
+// ==/UserScript==
